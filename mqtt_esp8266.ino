91a5b66243840b13cc9541c5a8feec9157b8c868
unknown <warlock.unleash@gmail.com>
2017-08-16 15:28:31 +0530

    esp8266 modification

--- mqtt_esp8266.ino
+++ mqtt_esp8266.ino
@@ -2,13 +2,17 @@
 #include <PubSubClient.h>
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>
+/* -----------------------------------------
+library below is used for saving router wifi and pass. Also one can change ssid and pass if router is changed via mobile.
+You can look into this library in github for more info. I have not used this library.
+-------------------------------------------- */
+#include <WiFiManager.h> 
 
 // Update these with values suitable for your network.
 
-const char* ssid = "CRYSTAL BROADBAND 52";
-const char* password = "8127743728";
-const char* mqtt_server = "m11.cloudmqtt.com";
+const char* ssid = "*********"; // Your wifi SSID
+const char* password = "***********"; // Your wifi Password
+const char* mqtt_server = "m11.cloudmqtt.com"; //You can input the name or IP of your mqtt broker.
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
@@ -23,9 +27,9 @@ void setup() {
   delay(1000);
   digitalWrite(LED,LOW);
   Serial.begin(115200);
-  //wifiManager.autoConnect("AP-NAME", "hello@123");
+  //wifiManager.autoConnect("SSID", "Password"); just used for demonstration.
   setup_wifi();
-  client.setServer(mqtt_server, 19845);
+  client.setServer(mqtt_server, 19845); // here 19845 is the port No.
   client.setCallback(callback);
 }
 
@@ -60,11 +64,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println();
 
   // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == 'O' && (char)payload[1]=='f') {
+  if ((char)payload[0] == 'O' && (char)payload[1]=='f') {   // I used Off for switcing On the light (led on node mcu is negative bias)
     digitalWrite(LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
     // but actually the LED is on; this is because
     // it is acive low on the ESP-01)
-  } else {
+  } else { 
     digitalWrite(LED, HIGH);  // Turn the LED off by making the voltage HIGH
   }
 
@@ -75,12 +79,12 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("Altamash","munnvxsn","KYuAfwQxGVpY")) {
+    if (client.connect("ID","(username","password")) { // input these values if your mqtt broker provide these info they make connection secure.
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("outTopic", "hello");
+      client.publish("outTopic", "hello"); // hello is the publishing topic
       // ... and resubscribe
-      client.subscribe("hello/world");
+      client.subscribe("hello/world"); // hello/world subscription topic
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
